bb6d9024b5995ef47a9a6185f01dfbc8fb3d1c9a
Sean McAllister <smcallis@google.com>
2022-11-20 11:09:57 -0700

    Enable thread safety analysis and cleanup any issues found.

--- inc/fzcw/fzcw.h
+++ inc/fzcw/fzcw.h
@@ -1,9 +1,14 @@
+// Copyright 2022 Google LLC
+// Author: Sean McAllister
+
 #pragma once
 
+#include <atomic>
 #include <algorithm>
 #include <memory>
 
 // abseil
+#include <absl/container/flat_hash_map.h>
 #include <absl/synchronization/mutex.h>
 
 // linux
@@ -65,8 +70,57 @@ private:
         }
     };
 
+    // A set of 64-bit offsets that can be updated from multiple threads.
+    //
+    // Individual offsets may be updated under a reader lock, but if we have to
+    // recompute the minimum offset, an exclusive lock is obtained to prevent
+    // further updates while the new value is computed.
+    //
+    // The current minimum can be read via min_offset() which doesn't take out a
+    // lock, it instead uses an atomic to provide a memory fence so that partial
+    // updates are never seen.
+    //
+    // The lock we use to synchronize access for writing the min read offset can
+    // also be used to wait for more data to come in.  await_bytes() will take
+    // out the lock and Await on it until the minimum offsets moves far enough
+    // forward.  We use conditional critical sections for this which should be
+    // more efficient than a condition variable.
+    struct ConcurrentPositionSet {
+        ConcurrentPositionSet()
+            : oneup_cnt_(0) {}
+
+        // Adds a new offset to the position set, set to the current minimum.
+        //
+        // Returns an integer identifier for the new offset.
+        int add_offset() LOCKS_EXCLUDED(lock_);
+
+        // Remove the given offset from the position set.
+        void del_offset(int id) LOCKS_EXCLUDED(lock_);
+
+        // Increments the given offset by some number of bytes.
+        void inc_offset(int id, int nbytes) LOCKS_EXCLUDED(lock_);
+
+        // Waits for a given number of bytes to become available.
+        //
+        // Returns true if the bytes became available or false if all offsets
+        // we were tracking were removed.
+        bool await_bytes(int nbytes) const LOCKS_EXCLUDED(lock_);
+
+        // Returns the current minimum offset value.
+        uint64_t min_offset() const {
+            return min_offset_.load(std::memory_order_acquire);
+        }
+
+    private:
+        mutable absl::Mutex lock_;
+        absl::flat_hash_map<int, uint64_t> offsets_ GUARDED_BY(lock_);
+        int oneup_cnt_                              GUARDED_BY(lock_);
+        std::atomic<uint64_t> min_offset_;
+    };
+
     // Create a new memory mapping of the given size.
     bool mmap_buffer(size_t size);
 
     MappedSizePtr buffer_;
+    ConcurrentPositionSet readers_;
 };
--- lib/fzcw.cc
+++ lib/fzcw.cc
@@ -1,4 +1,9 @@
+// Copyright 2022 Google LLC
+// Author: Sean McAllister
+
 #include <fzcw/fzcw.h>
+
+#include <limits>
 
 bool zcstream::mmap_buffer(size_t size) {
     int fd = memfd_create("zcstream", 0);
@@ -37,4 +42,67 @@ bool zcstream::mmap_buffer(size_t size) {
 
     buffer_ = MappedSizePtr(ptr, size, 2*size);
     return true;
+}
+
+int zcstream::ConcurrentPositionSet::add_offset() {
+    absl::WriterMutexLock lock(&lock_);
+    offsets_.emplace(oneup_cnt_, min_offset());
+    return oneup_cnt_++;
+}
+
+void zcstream::ConcurrentPositionSet::del_offset(int id) {
+    absl::WriterMutexLock lock(&lock_);
+    offsets_.erase(id);
+}
+
+void zcstream::ConcurrentPositionSet::inc_offset(int id, int nbytes) {
+    lock_.ReaderLock();
+
+    auto iter = offsets_.find(id);
+    if (iter == offsets_.end()) {
+        lock_.ReaderUnlock();
+        return;
+    }
+
+    uint64_t old_offset = iter->second;
+    iter->second += nbytes;
+
+    // If our old offset matches the minimum offset we might have been the
+    // bottleneck, update the minimum offset value.
+    if (old_offset == min_offset()) {
+        // Upgrade to an exclusive lock.
+        lock_.ReaderUnlock();
+        lock_.WriterLock();
+
+        // Make sure minimum wasn't updated while we waited.
+        if (old_offset == min_offset()) {
+            uint64_t min_offset = std::numeric_limits<uint64_t>::max();
+            for (const auto pair : offsets_) {
+                min_offset = std::min(min_offset, pair.second);
+            }
+            min_offset_.store(min_offset, std::memory_order_release);
+        }
+
+        // Releasing lock will wake any threads waiting on data.
+        lock_.WriterUnlock();
+        return;
+    }
+    lock_.ReaderUnlock();
+}
+
+bool zcstream::ConcurrentPositionSet::await_bytes(int nbytes) const {
+    lock_.ReaderLock();
+
+    uint64_t old_offset = min_offset();
+    auto bytes_ready = [this,old_offset,nbytes]() {
+        lock_.AssertReaderHeld();
+        return (min_offset() - old_offset >= nbytes) || offsets_.empty();
+    };
+    lock_.Await(absl::Condition(&bytes_ready));
+
+    // We stopped either because we got bytes or all the offsets were removed,
+    // return false in the second case.
+    bool empty = offsets_.empty();
+    lock_.ReaderUnlock();
+    return !empty;
 }
