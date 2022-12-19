f0c88e622efd2a37657b4cf414dbdaafd2f78d4d
Sean McAllister <smcallis@google.com>
2022-12-18 19:46:31 -0700

    Add documentation to spinlock.h

--- inc/skink/spinlock.h
+++ inc/skink/spinlock.h
@@ -3,6 +3,18 @@
 #include <atomic>
 #include "absl/base/thread_annotations.h"
 
+// A test-and-test-and-set (TATAS) spinlock.  The nested load checks when the
+// lock _might_ be available before attempting to acquire it.  Since the cache
+// coherency protocol can service multiple readers, this prevents the atomic
+// exchange operation from continuously changing cores.
+//
+// Spinlocks in general shouldn't be used in userspace since the kernel is
+// unaware when a thread is holding a spin lock and can deschedule it at any
+// time, blocking other waiting threads.
+//
+// They should be used very rare for very small critical sections that need to
+// be serialized.
+
 struct ABSL_LOCKABLE spinlock {
     spinlock() = default;
 
