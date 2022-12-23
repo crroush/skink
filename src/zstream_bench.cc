6135a0bc1ebb90ffb825a3fe7dce0a05968dc2fa
Sean McAllister <smcallis@gmail.com>
2022-12-23 14:36:50 -0700

    Add benchmarks for borrow interface.
    These don't actually move data, instead they're a test of how scalable
    the implementation is vs number of readers.

--- src/zstream_bench.cc
+++ src/zstream_bench.cc
@@ -73,6 +73,7 @@ ssize_t reader(int id, zstream &stream, ssize_t nbyte) {
 static inline void benchmark() {
   constexpr ssize_t nbyte = 1ull << 34;  // 16GB
 
+  printf("# benchmarking read/write API\n");
   printf("# bufsize  act_size  nreader  write (B/s)  read (B/s)  passed?\n");
   for (int i = 12; i <= 23; ++i) {
     const ssize_t bufsize = 1ull << i;
@@ -114,8 +115,110 @@ static inline void benchmark() {
   }
 }
 
+// Writes a given amount of bytes to the stream, returns number written.
+ssize_t writer_borrow(zstream &stream, ssize_t nbyte) {
+  // writer thread goes on CPU 3
+  cpu_set_t cpus;
+  CPU_ZERO(&cpus);
+  CPU_SET(3, &cpus);
+  sched_setaffinity(0, sizeof(cpu_set_t), &cpus);
+
+  ssize_t remain = nbyte;
+  while (remain) {
+    ssize_t nwrite = std::min(remain, (ssize_t)32768);
+
+    void* ptr = stream.wborrow(nwrite);
+    if (ptr == nullptr) {
+      break;
+    }
+    stream.wrelease(nwrite);
+    remain -= nwrite;
+
+  }
+  stream.wrclose();
+  return nbyte - remain;
+}
+
+// Reads a given amount of bytes from the stream, returns number read.
+ssize_t reader_borrow(int id, zstream &stream, ssize_t nbyte) {
+  // reader threads go on CPUs 4 on
+  cpu_set_t cpus;
+  CPU_ZERO(&cpus);
+  CPU_SET(4 + id, &cpus);
+  sched_setaffinity(0, sizeof(cpu_set_t), &cpus);
+
+  ssize_t remain = nbyte;
+  while (remain) {
+    ssize_t size  = std::min(remain, (ssize_t)32768);
+
+    sizeptr<const void> ptr = stream.rborrow(id, size);
+    if (!ptr) {
+      break;
+    }
+    stream.rrelease(id, size);
+    remain -= size;
+  }
+  stream.del_reader(id);
+  return nbyte - remain;
+}
+
+static inline void benchmark_borrow() {
+  constexpr ssize_t nbyte = 1ull << 34;  // 16GB
+
+  printf("# benchmarking borrow API\n");
+  printf("# bufsize  act_size  nreader  write (B/s)  read (B/s)  passed?\n");
+
+  for (int i = 12; i <= 23; ++i) {
+    const ssize_t bufsize = 1ull << i;
+    bool passed;
+    for (ssize_t nreader = 1; nreader <= 8; ++nreader) {
+      zstream stream(bufsize);
+      stream.set_spin_limit(1000000);
+
+      // Create readers.
+      double tstart = stopwatch();
+      std::vector<std::future<ssize_t>> results;
+      for (ssize_t ii = 0; ii < nreader; ii++) {
+        results.emplace_back(std::async(std::launch::async,
+                                        reader_borrow,
+                                        stream.add_reader(),
+                                        std::ref(stream),
+                                        nbyte));
+      }
+
+      // Create writers.
+      std::future<ssize_t> resultw =
+          std::async(std::launch::async, writer_borrow, std::ref(stream), nbyte);
+
+      passed = true;
+      for (ssize_t ii = 0; ii < nreader; ii++) {
+        passed &= (results[ii].get() == nbyte);
+      }
+
+      double telapsed = stopwatch(tstart);
+      printf("%8zd %8zd %zd %.9e %.9e %d\n",
+             bufsize,
+             stream.size(),
+             nreader,
+             nbyte / telapsed,
+             nreader * nbyte / telapsed,
+             passed);
+      fflush(stdout);
+    }
+  }
+}
+
+
 int main(int argc, char *argv[]) {
   CLI::App app{ "Simple FCZW" };
+
+  bool borrow = false;
+  app.add_flag("-b,--borrow", borrow, "Use borrow API instead of read/write");
+
   CLI11_PARSE(app, argc, argv);
-  benchmark();
+  if (borrow) {
+    benchmark_borrow();
+  } else {
+    benchmark();
+  }
 }
